d6d215bf78e663d0b324178d2c2bd69d0087e9a6
KMakohon <33064988+KMakohon@users.noreply.github.com>
2018-11-16 14:59:43 +0100

    teraz działa

--- serwer.c
+++ serwer.c
@@ -7,27 +7,23 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+#include <time.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
-struct sockaddr_in endpoint;
-FILE *flog;
-char myhostname[1024];
-char pnazwa[20];
 
-int main(int argc, char **argv) 
+char myhostname[1024];
+struct sockaddr_in soc;
+FILE *flog;
+
+int main() 
 {
   int sdServerSocket, sdConnection, retval;
   socklen_t sin_size;
   struct sockaddr_in incoming;
   struct hostent *heLocalHost;
-  char sign;
-  int lnIn1;
-  int sterujaca;
-  char odp;
-  char nazwa[20];
 
   sin_size = sizeof(struct sockaddr_in);
 
@@ -35,14 +31,20 @@ int main(int argc, char **argv)
   gethostname(myhostname, 1023);
   heLocalHost = gethostbyname(myhostname);
 
-  endpoint.sin_family = AF_INET;
-  endpoint.sin_port = htons(5002);
-  endpoint.sin_addr = *(struct in_addr*) 
-  heLocalHost->h_addr;
-  memset(&(endpoint.sin_zero),0,8);
-
   struct stat st = {0};
 
+  soc.sin_family = AF_INET;
+  soc.sin_port = htons(5007);
+  soc.sin_addr = *(struct in_addr*) 
+  heLocalHost->h_addr;
+  memset(&(soc.sin_zero),0,8);
+  char nazwa [20];
+
+  int signal;
+  int sterujaca;
+  char odp;
+
+//tworzenie plików
   if (stat("./serv", &st) == -1) 
   {
     mkdir("./serv", 0777);
@@ -51,12 +53,13 @@ int main(int argc, char **argv)
   {
     mkdir("./serv/pom", 0777);
   }
+//tworzednie plików
 
   printf("slucham na %s : %d\n",
-  inet_ntoa(endpoint.sin_addr),
-  ntohs(endpoint.sin_port));
+  inet_ntoa(soc.sin_addr),
+  ntohs(soc.sin_port));
 
-  retval = bind(sdServerSocket, (struct sockaddr*) &endpoint, sizeof(struct sockaddr));
+  retval = bind(sdServerSocket, (struct sockaddr*) &soc, sizeof(struct sockaddr));
   if (retval < 0) 
   {
     printf("bind nie powiodl sie\n");
@@ -64,23 +67,20 @@ int main(int argc, char **argv)
   }
 
   listen(sdServerSocket, 10);
-
-  sin_size = sizeof(struct sockaddr_in);
+    int abcd;
   while ((sdConnection = accept(sdServerSocket, (struct sockaddr*) &incoming, &sin_size)) > 0) 
   {
+    memset(&signal, 0, sizeof(int));
     printf("Polaczenie z %s:%d\n",
     inet_ntoa(incoming.sin_addr),
     ntohs(incoming.sin_port));
-  
-  if (recv(sdConnection, &lnIn1, sizeof(int),0) != sizeof(int)) 
-  {
-    printf("pierwszy recv nie powiodl sie. \n");
-    close(sdConnection);
-    continue;
-  }
 
-  sterujaca = htonl(lnIn1);
-  switch(sterujaca)
+if (recv(sdConnection, &signal, sizeof(signal),0) == 4)
+{
+
+print("%d \n", signal)
+/*
+switch(signal)
   {
     case 1: 
     printf("1\n");
@@ -125,7 +125,20 @@ int main(int argc, char **argv)
     default:  
     close(sdConnection);
     break;
-  }
+}
+}
+
+//  {
+//    printf("pierwszy recv nie powiodl sie. \n");
+//    close(sdConnection);
+//    continue;
+//  }
+
+//  sterujaca = htonl(signal);
+
+*/
+}
+    close(sdConnection);
 }
 return 0;
 }
