fe2e5b251246f6eb578dc2ea2dd5347f55c92448
Ikarani <ikarani24@op.pl>
2018-11-23 14:14:34 +0100

    Add files via upload

---
+++ serwer/twój serwer/kompresja.h
@@ -0,0 +1,22 @@
+#ifndef _KOMPRESJA_H
+#define _KOMPRESJA_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <time.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int kom();
+void czyt();
+ 
+#endif
